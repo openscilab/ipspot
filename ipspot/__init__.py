86d0f25425c4c3bd135054da80a07567697465df
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-04-25 00:06:39 +0330

    base

---
+++ ipspot/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""ipspot modules."""
+from ipspot.params import IPSPOT_VERSION, IPv4API
+from ipspot.functions import get_private_ipv4, get_public_ipv4
+__version__ = IPSPOT_VERSION
