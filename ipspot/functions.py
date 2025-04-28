cafc4b15f4e27386dfcf6c0eb29c9c986a84b496
Sadra Sabouri <43045767+sadrasabouri@users.noreply.github.com>
2025-04-28 13:59:36 -0700

    `--timeout` parameter added (#13)
    * add : `timeout` parameter added to all functions.py modules.
    
    * log : changes are logged.
    
    * update : readme updated.
    
    * fix : timeout actually working now.
    
    * fix : timeout type-hint fixed.
    
    * log : changes logged.

--- ipspot/functions.py
+++ ipspot/functions.py
@@ -2,7 +2,7 @@
 """ipspot functions."""
 import argparse
 import socket
-from typing import Union, Dict, Any
+from typing import Union, Dict, Tuple, Any
 import requests
 from art import tprint
 from .params import REQUEST_HEADERS, IPv4API, PARAMETERS_NAME_MAP
@@ -27,14 +27,15 @@ def get_private_ipv4() -> Dict[str, Union[bool, Dict[str, str], str]]:
         return {"status": False, "error": str(e)}
 
 
-def _ipsb_ipv4(geo: bool=False) -> Dict[str, Union[bool, Dict[str, Union[str, float]], str]]:
+def _ipsb_ipv4(geo: bool=False, timeout: Union[float, Tuple[float, float]]=5) -> Dict[str, Union[bool, Dict[str, Union[str, float]], str]]:
     """
     Get public IP and geolocation using ip.sb.
 
     :param geo: geolocation flag
+    :param timeout: timeout value for API
     """
     try:
-        response = requests.get("https://api.ip.sb/geoip", headers=REQUEST_HEADERS, timeout=5)
+        response = requests.get("https://api.ip.sb/geoip", headers=REQUEST_HEADERS, timeout=timeout)
         response.raise_for_status()
         data = response.json()
         result = {"status": True, "data": {"ip": data.get("ip"), "api": "ip.sb"}}
@@ -55,14 +56,15 @@ def _ipsb_ipv4(geo: bool=False) -> Dict[str, Union[bool, Dict[str, Union[str, fl
         return {"status": False, "error": str(e)}
 
 
-def _ipapi_ipv4(geo: bool=False) -> Dict[str, Union[bool, Dict[str, Union[str, float]], str]]:
+def _ipapi_ipv4(geo: bool=False, timeout: Union[float, Tuple[float, float]]=5) -> Dict[str, Union[bool, Dict[str, Union[str, float]], str]]:
     """
     Get public IP and geolocation using ip-api.com.
 
     :param geo: geolocation flag
+    :param timeout: timeout value for API
     """
     try:
-        response = requests.get("http://ip-api.com/json/", timeout=5)
+        response = requests.get("http://ip-api.com/json/", timeout=timeout)
         response.raise_for_status()
         data = response.json()
 
@@ -86,14 +88,15 @@ def _ipapi_ipv4(geo: bool=False) -> Dict[str, Union[bool, Dict[str, Union[str, f
         return {"status": False, "error": str(e)}
 
 
-def _ipinfo_ipv4(geo: bool=False) -> Dict[str, Union[bool, Dict[str, Union[str, float]], str]]:
+def _ipinfo_ipv4(geo: bool=False, timeout: Union[float, Tuple[float, float]]=5) -> Dict[str, Union[bool, Dict[str, Union[str, float]], str]]:
     """
     Get public IP and geolocation using ipinfo.io.
 
     :param geo: geolocation flag
+    :param timeout: timeout value for API
     """
     try:
-        response = requests.get("https://ipinfo.io/json", timeout=5)
+        response = requests.get("https://ipinfo.io/json", timeout=timeout)
         response.raise_for_status()
         data = response.json()
         result = {"status": True, "data": {"ip": data.get("ip"), "api": "ipinfo.io"}}
@@ -116,12 +119,14 @@ def _ipinfo_ipv4(geo: bool=False) -> Dict[str, Union[bool, Dict[str, Union[str, 
 
 
 def get_public_ipv4(api: IPv4API=IPv4API.AUTO,
-                    geo: bool=False) -> Dict[str, Union[bool, Dict[str, Union[str, float]], str]]:
+                    geo: bool=False,
+                    timeout: Union[float, Tuple[float, float]]=5) -> Dict[str, Union[bool, Dict[str, Union[str, float]], str]]:
     """
     Get public IPv4 and geolocation info based on the selected API.
 
     :param api: public IPv4 API
     :param geo: geolocation flag
+    :param timeout: timeout value for API
     """
     api_map = {
         IPv4API.IPAPI: _ipapi_ipv4,
@@ -131,14 +136,14 @@ def get_public_ipv4(api: IPv4API=IPv4API.AUTO,
 
     if api == IPv4API.AUTO:
         for _, func in api_map.items():
-            result = func(geo=geo)
+            result = func(geo=geo, timeout=timeout)
             if result["status"]:
                 return result
         return {"status": False, "error": "All attempts failed."}
     else:
         func = api_map.get(api)
         if func:
-            return func(geo=geo)
+            return func(geo=geo, timeout=timeout)
         return {"status": False, "error": "Unsupported API: {api}".format(api=api)}
 
 
@@ -155,12 +160,13 @@ def filter_parameter(parameter: Any) -> Any:
     return parameter
 
 
-def display_ip_info(ipv4_api: IPv4API = IPv4API.AUTO, geo: bool=False) -> None:
+def display_ip_info(ipv4_api: IPv4API = IPv4API.AUTO, geo: bool=False, timeout: Union[float, Tuple[float, float]]=5) -> None:
     """
     Print collected IP and location data.
 
     :param ipv4_api: public IPv4 API
     :param geo: geolocation flag
+    :param timeout: timeout value for API
     """
     private_result = get_private_ipv4()
     print("Private IP:\n")
@@ -172,7 +178,7 @@ def display_ip_info(ipv4_api: IPv4API = IPv4API.AUTO, geo: bool=False) -> None:
         public_title += " and Location Info"
     public_title += ":\n"
     print(public_title)
-    public_result = get_public_ipv4(ipv4_api, geo=geo)
+    public_result = get_public_ipv4(ipv4_api, geo=geo, timeout=timeout)
     if public_result["status"]:
         for name, parameter in sorted(public_result["data"].items()):
             print(
@@ -196,6 +202,7 @@ def main() -> None:
     parser.add_argument('--info', help='info', nargs="?", const=1)
     parser.add_argument('--version', help='version', nargs="?", const=1)
     parser.add_argument('--no-geo', help='no geolocation data', nargs="?", const=1, default=False)
+    parser.add_argument('--timeout', help='timeout for the API request', type=float, default=5.0)
 
     args = parser.parse_args()
     if args.version:
@@ -205,4 +212,4 @@ def main() -> None:
     else:
         ipv4_api = IPv4API(args.ipv4_api)
         geo = not args.no_geo
-        display_ip_info(ipv4_api=ipv4_api, geo=geo)
+        display_ip_info(ipv4_api=ipv4_api, geo=geo, timeout=args.timeout)
