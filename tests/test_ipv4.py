5dc1d3bb79e9aabeb407d28d46a91510defd3ec3
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-05-02 19:17:03 +0330

    fix : tests updated

--- tests/test_ipv4.py
+++ tests/test_ipv4.py
@@ -1,4 +1,5 @@
 import re
+from unittest import mock
 from ipspot import get_private_ipv4
 from ipspot import get_public_ipv4, IPv4API
 
@@ -7,25 +8,32 @@ IPV4_REGEX = re.compile(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')
 DATA_ITEMS = {'country_code', 'latitude', 'longitude', 'api', 'country', 'timezone', 'organization', 'region', 'ip', 'city'}
 
 
-def test_private_ipv4():
+def test_private_ipv4_success():
     result = get_private_ipv4()
     assert result["status"]
     assert IPV4_REGEX.match(result["data"]["ip"])
 
 
-def test_public_ipv4_auto1():
+def test_private_ipv4_error():
+    with mock.patch("socket.gethostbyname", side_effect=Exception("Test error")):
+        result = get_private_ipv4()
+        assert not result["status"]
+        assert result["error"] == "Test error"
+
+
+def test_public_ipv4_auto_success():
     result = get_public_ipv4(api=IPv4API.AUTO, geo=True)
     assert result["status"]
     assert IPV4_REGEX.match(result["data"]["ip"])
     assert set(result["data"].keys()) == DATA_ITEMS
 
 
-def test_public_ipv4_auto2():
+def test_public_ipv4_auto_error():
     result = get_public_ipv4(api=IPv4API.AUTO, geo=True, timeout="5")
     assert not result["status"]
 
 
-def test_public_ipv4_ipapi1():
+def test_public_ipv4_ipapi_success():
     result = get_public_ipv4(api=IPv4API.IPAPI, geo=True)
     assert result["status"]
     assert IPV4_REGEX.match(result["data"]["ip"])
@@ -33,12 +41,12 @@ def test_public_ipv4_ipapi1():
     assert result["data"]["api"] == "ip-api.com"
 
 
-def test_public_ipv4_ipapi2():
+def test_public_ipv4_ipapi_error():
     result = get_public_ipv4(api=IPv4API.IPAPI, geo=True, timeout="5")
     assert not result["status"]
 
 
-def test_public_ipv4_ipinfo1():
+def test_public_ipv4_ipinfo_success():
     result = get_public_ipv4(api=IPv4API.IPINFO, geo=True)
     assert result["status"]
     assert IPV4_REGEX.match(result["data"]["ip"])
@@ -46,12 +54,12 @@ def test_public_ipv4_ipinfo1():
     assert result["data"]["api"] == "ipinfo.io"
 
 
-def test_public_ipv4_ipinfo2():
+def test_public_ipv4_ipinfo_error():
     result = get_public_ipv4(api=IPv4API.IPINFO, geo=True, timeout="5")
     assert not result["status"]
 
 
-def test_public_ipv4_ipsb1():
+def test_public_ipv4_ipsb_success():
     result = get_public_ipv4(api=IPv4API.IPSB, geo=True)
     assert result["status"]
     assert IPV4_REGEX.match(result["data"]["ip"])
@@ -59,12 +67,12 @@ def test_public_ipv4_ipsb1():
     assert result["data"]["api"] == "ip.sb"
 
 
-def test_public_ipv4_ipsb2():
+def test_public_ipv4_ipsb_error():
     result = get_public_ipv4(api=IPv4API.IPSB, geo=True, timeout="5")
     assert not result["status"]
 
 
-def test_public_ipv4_error():
+def test_public_ipv4_api_error():
     result = get_public_ipv4(api="api1", geo=True)
     assert not result["status"]
     assert result["error"] == "Unsupported API: api1"
