9d6610c2211f81117044841539431198bfb0a315
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-08-20 21:18:56 +0330

    fix : test_ipv6_api.py modified

--- tests/test_ipv6_api.py
+++ tests/test_ipv6_api.py
@@ -6,14 +6,14 @@ DATA_ITEMS = {'country_code', 'latitude', 'longitude', 'api', 'country', 'timezo
 
 
 def test_public_ipv6_auto_success():
-    result = get_public_ipv6(api=IPv6API.AUTO, geo=True)
+    result = get_public_ipv6(api=IPv6API.AUTO, geo=True, timeout=40, max_retries=4, retry_delay=90)
     assert result["status"]
     assert is_ipv6(result["data"]["ip"])
     assert set(result["data"].keys()) == DATA_ITEMS
 
 
 def test_public_ipv6_auto_safe_success():
-    result = get_public_ipv6(api=IPv6API.AUTO_SAFE, geo=True)
+    result = get_public_ipv6(api=IPv6API.AUTO_SAFE, geo=True, timeout=40, max_retries=4, retry_delay=90)
     assert result["status"]
     assert is_ipv6(result["data"]["ip"])
     assert set(result["data"].keys()) == DATA_ITEMS
