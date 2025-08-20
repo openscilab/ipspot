6043942a2c3292cbb4fdb6eee4cb9dfb9f88b2c1
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-08-20 20:51:31 +0330

    fix : test_ipv6_api.py added

---
+++ tests/test_ipv6_api.py
@@ -0,0 +1,19 @@
+from ipspot import get_public_ipv6, is_ipv6
+from ipspot import IPv6API
+
+TEST_CASE_NAME = "IPv6 API tests"
+DATA_ITEMS = {'country_code', 'latitude', 'longitude', 'api', 'country', 'timezone', 'organization', 'region', 'ip', 'city'}
+
+
+def test_public_ipv6_auto_success():
+    result = get_public_ipv6(api=IPv6API.AUTO, geo=True)
+    assert result["status"]
+    assert is_ipv6(result["data"]["ip"])
+    assert set(result["data"].keys()) == DATA_ITEMS
+
+
+def test_public_ipv6_auto_safe_success():
+    result = get_public_ipv6(api=IPv6API.AUTO_SAFE, geo=True)
+    assert result["status"]
+    assert is_ipv6(result["data"]["ip"])
+    assert set(result["data"].keys()) == DATA_ITEMS
