eb743729341bd10349e4f46bdcf029187497f6dd
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-12-16 17:27:59 +0330

    fix : tests updated

--- tests/test_ipv6_api.py
+++ tests/test_ipv6_api.py
@@ -96,3 +96,11 @@ def test_public_ipv6_auto_safe_success():
     assert result["status"]
     assert is_ipv6(result["data"]["ip"])
     assert set(result["data"].keys()) == DATA_ITEMS
+
+
+def test_public_ipv6_myip_wtf_success():
+    result = get_public_ipv6(api=IPv4API.MYIP_WTF, geo=True, timeout=40, max_retries=4, retry_delay=90, backoff_factor=1.1)
+    assert result["status"]
+    assert is_ipv6(result["data"]["ip"])
+    assert set(result["data"].keys()) == DATA_ITEMS
+    assert result["data"]["api"] == "myip.wtf"
